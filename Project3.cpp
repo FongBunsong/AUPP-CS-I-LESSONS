355f622b50f82f14b6eb3801e0299b0e815f224b
Fong Bunsong <2023276fong@aupp.edu.kh>
2024-10-18 13:26:55 +0700

    homework project

--- Project3.cpp
+++ Project3.cpp
@@ -1,74 +1,60 @@
 #include <iostream>
-
 using namespace std;
 
-// Function to validate human's input
-int getHumanMove(int toothpicks) {
-    int move;
-    do {
-        cout << "Enter number of toothpicks to withdraw (1, 2, or 3): ";
-        cin >> move;
-        if (move < 1 || move > 3 || move > toothpicks) {
-            cout << "Invalid move. Try again." << endl;
-        }
-    } while (move < 1 || move > 3 || move > toothpicks);
-    return move;
-}
-
-// Function for computer's move
-int getComputerMove(int toothpicks, int humanMove) {
-    int move;
-    if (toothpicks > 4) {
-        move = 4 - humanMove;
-    } else if (toothpicks >= 2 && toothpicks <= 4) {
-        move = toothpicks - 1;
-    } else {
-        move = 1;
-    }
-    return move;
-}
-
-// Function to play the game
-void playGame() {
-    int toothpicks = 23;
-    int humanMove, computerMove;
-
-    cout << "Welcome to the game of 23!" << endl;
-
-    while (toothpicks > 0) {
-        // Human's turn
-        cout << "Toothpicks remaining: " << toothpicks << endl;
-        humanMove = getHumanMove(toothpicks);
-        toothpicks -= humanMove;
-        if (toothpicks == 0) {
-            cout << "Human picked the last toothpick. Human loses!" << endl;
-            break;
-        }
-
-        // Computer's turn
-        computerMove = getComputerMove(toothpicks, humanMove);
-        toothpicks -= computerMove;
-        cout << "Computer withdraws " << computerMove << " toothpick(s)." << endl;
-        if (toothpicks == 0) {
-            cout << "Computer picked the last toothpick. Computer loses!" << endl;
-            break;
-        }
-    }
-}
-
-// Main function
 int main() {
     char playAgain;
-
     do {
-        playGame();
-        cout << "Do you want to play again? (y/n): ";
-        cin >> playAgain;
-    } while (playAgain == 'y' || playAgain == 'Y');
+        int sticks = 23; // Initial number of sticks
+        int playerPick, computerPick;
 
-    cout << "Thank you for playing!" << endl;
+        cout << "Let's play a game of \"23\"!" << endl;
+
+        while (sticks > 0) {
+            // Player's turn
+            cout << "Enter the number of sticks you wish to pick (1, 2, or 3): ";
+            cin >> playerPick;
+
+            // Input validation for player's pick
+            while (playerPick < 1 || playerPick > 3 || playerPick > sticks) {
+                cout << "Wrong number of sticks. Please pick 1, 2, or 3 sticks: ";
+                cin >> playerPick;
+            }
+
+            // Update the number of sticks
+            sticks -= playerPick;
+            cout << "You picked " << playerPick << " stick" << (playerPick > 1 ? "s" : "") << ". " << sticks << " left." << endl;
+
+            // Check if the player picked the last stick
+            if (sticks == 0) {
+                cout << "You picked the last stick." << endl;
+                cout << "Sorry, the computer beat you!" << endl;
+                break;
+            }
+
+            // Computer's turn
+            if (sticks > 4) {
+                computerPick = 4 - playerPick;
+            } else {
+                computerPick = sticks - 1;
+            }
+
+            // Update the number of sticks
+            sticks -= computerPick;
+            cout << "Computer picked " << computerPick << " stick" << (computerPick > 1 ? "s" : "") << ". " << sticks << " left." << endl;
+
+            // Check if the computer picked the last stick
+            if (sticks == 0) {
+                cout << "Computer picked the last stick." << endl;
+                cout << "Congratulations! You beat the computer!" << endl;
+                break;
+            }
+        }
+
+        // Prompt to play again
+        cout << "Do you want to play another game? <Y/N>: ";
+        cin >> playAgain;
+
+    } while (playAgain == 'Y' || playAgain == 'y');
+
     return 0;
 }
-
-// this is just a test. I will do it again before submitting it to professor. 
-// Thank You. 
